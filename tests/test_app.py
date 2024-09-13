618475d505f07f4b07df819d985732d3bde5bade
leesahanders <lisa.anders@posit.co>
2024-09-13 14:55:46 -0600

    try12

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,7 @@ from playwright.sync_api import Page
 from shiny.playwright import controller
 from shiny.pytest import create_app_fixture
 from shiny.run import ShinyAppProc
-from string import punctuation
 import pytest
-
-#set(punctuation)
 
 #app = create_app_fixture("..\app.py") # for Linux/Mac
 #app = create_app_fixture("../app.py") # For Windows
