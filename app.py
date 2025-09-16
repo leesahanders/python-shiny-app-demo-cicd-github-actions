725a3b3ee245ce14e934d3c7fb348020a49ae533
leesahanders <lisamaeanders@gmail.com>
2025-09-16 19:32:56 +0000

    sync test

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 # Shiny express app, read more here: https://shiny.posit.co/blog/posts/shiny-express/
 # Run it locally with: shiny run --reload app.py
 from shiny.express import input, render, ui
-from palmerpenguins import load_penguins
-
-penguins = load_penguins()
 
 ui.panel_title("Hello Shiny!")
 ui.input_slider("n", "N", 0, 100, 20)
